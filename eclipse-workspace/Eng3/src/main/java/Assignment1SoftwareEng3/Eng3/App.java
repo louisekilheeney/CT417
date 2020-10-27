c87a94e089731b22c58be0f840c52e3af573db30
Louise <louisekilheeney123@gmail.com>
2020-10-27 12:38:21 +0000

    Cleaning up file

--- eclipse-workspace/Eng3/src/main/java/Assignment1SoftwareEng3/Eng3/App.java
+++ eclipse-workspace/Eng3/src/main/java/Assignment1SoftwareEng3/Eng3/App.java
@@ -10,20 +10,3 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
-
-//Student student1 = new Student("Louise Kilheeney", 28, "17/04/1992", 12345, "1875772","programming");
-//student1.getUsername();
-//
-//ArrayList<Student> students = new ArrayList<Student>();
-//students.add(student1);
-//
-//
-//Module module1 = new Module("Soft Eng 3", "CT4117", students);
-//
-//ArrayList<Module> module = new ArrayList<Module>();
-//module.add(module1);
-//
-//DateTime start = new DateTime("2019-09-01");
-//DateTime end = new DateTime("2019-11-29");
-//
-//ProgrammingClass pg = new ProgrammingClass("CS&IT", module, students, start, end);
